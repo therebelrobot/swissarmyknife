6e2a76cf5ebb01932721bc427f05cafc296d2740
Trent Oswald <trentoswald@therebelrobot.com>
2013-04-23 16:04:17 -0600

    device detection

--- jquery.rrToolbelt.js
+++ jquery.rrToolbelt.js
@@ -8,6 +8,7 @@ https://github.com/therebelrobot/jquery.rrToolbelt
 
 Dependencies:
 	jQuery - //cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js
+	jquery.backGroundSize.js - - https://github.com/louisremi/jquery.backgroundSize.js
 	jquery.rrToolbelt.ua.xml - Normally included with this file
 
 Available functions:
@@ -30,74 +31,75 @@ Available functions:
 
 ****************************** */
 
-function $_GET(p) {
-	var url = window.location.search.substring(1);
-	var queryParams = url.split('&');
-	for (var x = 0; x < queryParams.length; x++) {
-		var paramSplit = queryParams[x].split('=');
-		if (paramSplit[0] == p) {
-			var value = paramSplit[1];
-			if (value.indexOf('#')>-1){
-				value = value.split("#");
-				value = value[0];
+var $_rootDir = 'js/';
+
+/*GET parameters*/
+	function $_GET(p) {
+		var url = window.location.search.substring(1);
+		var queryParams = url.split('&');
+		for (var x = 0; x < queryParams.length; x++) {
+			var paramSplit = queryParams[x].split('=');
+			if (paramSplit[0] == p) {
+				var value = paramSplit[1];
+				if (value.indexOf('#')>-1){
+					value = value.split("#");
+					value = value[0];
+				}
+				return value;
 			}
-			return value;
 		}
-	}
-};
-var $_deviceReady = false;
-var $_devXml = '';
-$.get('global.ua.xml',function(data){
-	$_devXml = data;
-}).fail(function(){
-	console.error('global.js error: Unable to retrieve global.ua.xml. Please ensure it is located in the same directory as global.js');
-}).done(function(){
-	$_deviceReady = true;
-});
-function $_device(){
-	if ($_deviceReady){
-		var profile = {
-			type: false,
-			name: false,
-			OS: false,
-			OSv: false,
-			browser: false,
-			bv: false,
-			view:{
-				width:false,
-				height:false
-			},
-			standalone:false
-		};
-		var ua = window.navigator.userAgent;
-		if (ua == ''){
-			return false;
-		}
-		if ($_devXml.indexOf(ua) < 0){
-			profile = {
-				type: 'unknown',
-				name: 'unknown',
-				OS: 'unknown',
-				OSv: 'unknown',
-				browser: 'unknown',
-				bv: 'unknown',
-				standalone:'unknown'
+	};
+
+/*IE8 Fixes*/
+
+	/*bg size*/
+
+	/*opacity*/
+		/*.transparent_class {
+		  -ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=50)"; // ie8
+
+		  filter: alpha(opacity=50); // ie5-7
+
+		  -moz-opacity: 0.5; // netscape
+
+		  -khtml-opacity: 0.5; // safari 1.x
+
+		  opacity: 0.5; // everyone else
+		}*/
+
+/*device functions*/
+	function $_device(){
+			var profile = {
+				type: false, // phone, tablet, desktop
+				OS: false, // windows, mac, android, linux, iPad
+				OSv: false, // return version of above OS
+				standalone:false, // true if iPad standalone browser
+				browser: false, // MSIE, firefox, chrome, safari, opera, other
+				bv: false, // return version of above OS
+				view:{ // viewport dimensions
+					width:false,
+					height:false
+				}
 			};
-		}
-		else{
-			var dev = $_devXml.find('useragent[useragent="'+ua+'"]');
+			/*Retrieve userAgent*/
+				var ua = window.navigator.userAgent;
+				if (ua == ''){
+					return false;
+				}
 
-		}
-		/*return viewport dimensions*/
-		var h = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-		var w = Math.max( body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth );
-		profile.view.height = h;
-		profile.view.width = w;
+			/*return viewport dimensions*/
+				var h = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+				var w = Math.max( body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth );
+				profile.view.height = h;
+				profile.view.width = w;
 
-		/*** return the final results ***/
-		return profile;
-	}
-	else{
-		return false;
-	}
-};
+			/*set type*/
+
+			/*set OS and OSv - along with standalone, if iPad*/
+
+			/*set browser and bv*/
+
+
+			/*** return the final results ***/
+			return profile;
+	};
